59351ec0cba765852a1992bbf19d5140745a39aa
HersonaREAL <jycdeya@gmail.com>
2019-04-09 21:53:32 +0800

    Site updated: 2019-04-09 21:53:32

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","77b15d522b854e9bab28a3d297ea56bf"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
